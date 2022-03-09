34bf18e3bfcb522f36d5f14cca01f244d634e8eb
erik schievenin <erik.schievenin@gmail.com>
2022-03-09 15:59:20 +0100

    Exercise start

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log('JS OK!')
+
+// esercizio 2
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per
+// il computer.
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+let x;
+
+for (let x = 0; x <= 6; x++) {
+
+    if (x > y) {
+        console.log('vince Player')
+
+    } else {
+        console.log('vince Computer')
+    }
+
+}
